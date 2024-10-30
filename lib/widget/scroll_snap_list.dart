2a35ba961a00b0b80042fbd5c5ae1d2bd3f6eb00
PangYu <646182805@qq.com>
2024-10-30 18:18:36 +0800

    城市列表添加上限

--- lib/widget/scroll_snap_list.dart
+++ lib/widget/scroll_snap_list.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:scrollview_observer/scrollview_observer.dart';
 
 ///Anchor location for selected item in the list
 enum SelectedItemAnchor { start, middle, end }
